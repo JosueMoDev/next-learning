2fdc253a3e300ce03796c85cc258e3eff6bcfa95
Jonas Morales <jonasjosuemo2021@outlook.com>
2024-01-10 15:10:40 -0600

    counter implemented

--- my-dashboard/src/app/dashboard/counter/page.tsx
+++ my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,15 @@
+import { CarCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Un simple contador",
+};
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CarCounter value={20} />
     </div>
   );
 }
