ec0b987b563bafa758620ec542bb83a08b6022b4
Jonas Morales <jonasjosuemo2021@outlook.com>
2024-01-22 14:39:54 -0600

    server side pagination of products, by gender and name pages refactor

--- teslo-shop/src/app/(shop)/page.tsx
+++ teslo-shop/src/app/(shop)/page.tsx
@@ -1,24 +1,33 @@
-import { ProductGrid, Title } from '@/components';
-import { initialData } from '@/seed/seed';
+export const revalidate = 60; // 60 segundos
 
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
 
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
-export default function Home() {
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
+
   return (
     <>
-      <Title
-        title="Tienda"
-        subtitle="Todos los productos"
-        className="mb-2"
-      />
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
 
-      <ProductGrid 
-        products={ products }
-      />
-      
+      <ProductGrid products={products} />
+
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
--- teslo-shop/src/seed/seed-database.ts
+++ teslo-shop/src/seed/seed-database.ts
@@ -6,63 +6,81 @@ import prisma from '../lib/prisma';
 
 async function main() {
 
+  // 1. Borrar registros previos
+  // await Promise.all( [
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  // ]);
+  
+  const { categories, products } = initialData;
 
-    await prisma.productImage.deleteMany();
-    await prisma.product.deleteMany();
-    await prisma.category.deleteMany();
 
-    const { categories, products } = initialData;
+  //  Categorias
+  // {
+  //   name: 'Shirt'
+  // }
+  const categoriesData = categories.map( (name) => ({ name }));
+  
+  await prisma.category.createMany({
+    data: categoriesData
+  });
 
-    const categoriesData = categories.map((name) => ({ name }));
+  
+  const categoriesDB = await prisma.category.findMany();
+  
+  const categoriesMap = categoriesDB.reduce( (map, category) => {
+    map[ category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>); //<string=shirt, string=categoryID>
+  
+  
 
-    await prisma.category.createMany({
-        data: categoriesData
+  // Productos
+
+  products.forEach( async(product) => {
+
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    })
+
+
+    // Images
+    const imagesData = images.map( image => ({
+      url: image,
+      productId: dbProduct.id
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData
     });
 
-
-    const categoriesDB = await prisma.category.findMany();
-
-    const categoriesMap = categoriesDB.reduce((map, category) => {
-        map[category.name.toLowerCase()] = category.id;
-        return map;
-    }, {} as Record<string, string>); //<string=shirt, string=categoryID>
+  });
 
 
 
-    // Productos
-
-    products.forEach(async (product) => {
-
-        const { type, images, ...rest } = product;
-
-        const dbProduct = await prisma.product.create({
-            data: {
-                ...rest,
-                categoryId: categoriesMap[type]
-            }
-        })
 
 
-        // Images
-        const imagesData = images.map(image => ({
-            url: image,
-            productId: dbProduct.id
-        }));
-
-        await prisma.productImage.createMany({
-            data: imagesData
-        });
-
-    });
-
-
-    console.log('Seed ejecutado correctamente');
+  console.log( 'Seed ejecutado correctamente' );
 }
 
 
 
-(() => {
 
-    if (process.env.NODE_ENV === 'production') return;
-    main();
-})();
+
+
+
+
+
+( () => {
+
+  if ( process.env.NODE_ENV === 'production' ) return;
+
+
+  main();
+} )();
